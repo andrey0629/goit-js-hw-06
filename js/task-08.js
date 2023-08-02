3aab979635c61f45523ec1c5606a4ac555c4c01f
andrey0629 <ANDRII.BRIEF@GMAIL.COM>
2023-08-02 12:06:30 +0200

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -18,3 +18,18 @@
 // Если в форме есть незаполненные поля, выводи alert с предупреждением о том, что все поля должны быть заполнены.
 // Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект, где имя поля будет именем свойства, а значение поля - значением свойства. Для доступа к элементам формы используй свойство elements.
 // Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
+
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const { elements: { email, password }} = event.currentTarget;
+  
+    if (email.value === "" || password.value === "") {
+    alert("Пожалуйста, заполните все поля формы!");
+    }
+    
+    console.log(`Email: ${email.value}, Password: ${password.value}`);
+  
+    event.currentTarget.reset();
+});
