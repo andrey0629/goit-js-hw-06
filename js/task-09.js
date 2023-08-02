12e629a14059226b873fd4a9078e322586ba2358
andrey0629 <ANDRII.BRIEF@GMAIL.COM>
2023-08-02 12:46:49 +0200

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,9 +1,3 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
-
 // Задание 9
 // Напиши скрипт, который изменяет цвета фона элемента <body> через инлайн стиль при клике на button.change-color и выводит значение цвета в span.color.
 
@@ -19,3 +13,18 @@ function getRandomHexColor() {
 //     .toString(16)
 //     .padStart(6, 0)}`;
 // }
+
+const body = document.querySelector("body");
+const color = document.querySelector(".color");
+const changeColor = document.querySelector(".change-color"); 
+
+function getRandomHexColor(event) {
+  body.style.backgroundColor =
+    `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+  
+  color.textContent = body.style.backgroundColor
+}
+
+changeColor.addEventListener("click", getRandomHexColor);
